cde56531464c6b667339c9d3ce891ca6eff8af1a
Quentin Valmori <quentin.valmori@gmail.com>
2016-06-03 08:35:53 +0200

    Working rnmk demo RN 0.26

---
+++ index.ios.js
@@ -0,0 +1,133 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  AppRegistry,
+  NavigatorIOS,
+  TouchableOpacity
+} from 'react-native'
+
+import { setTheme, MKColor } from 'react-native-material-kit';
+
+// customize the material design theme
+setTheme({
+  primaryColor: MKColor.Purple,
+  primaryColorRGB: MKColor.RGBPurple,
+  accentColor: MKColor.Amber,
+});
+
+var Buttons = require('./app/buttons');
+var TextFields = require('./app/textfields');
+var Toggles = require('./app/toggles');
+var Progress = require('./app/progress');
+var Sliders = require('./app/sliders');
+var Cards = require('./app/cards');
+
+var Home = React.createClass({
+  render: function () {
+   return (
+      <ScrollView style={styles.list}
+                  contentContainerStyle={styles.container}>
+        <TouchableOpacity onPress={() => {
+          this.props.navigator.push({
+            title: 'Buttons',
+            component: Buttons,
+          });
+        }}>
+          <Text style={styles.pushLabel}>Buttons</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {
+          this.props.navigator.push({
+            title: 'Cards',
+            component: Cards,
+          });
+        }}>
+          <Text style={styles.pushLabel}>Cards</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {
+          this.props.navigator.push({
+            title: 'Loading',
+            component: Progress,
+          });
+        }}>
+          <Text style={styles.pushLabel}>Loading</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {
+          this.props.navigator.push({
+            title: 'Sliders',
+            component: Sliders,
+          });
+        }}>
+          <Text style={styles.pushLabel}>Sliders</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {
+          this.props.navigator.push({
+            title: 'Text Fields',
+            component: TextFields,
+          });
+        }}>
+          <Text style={styles.pushLabel}>Text Fields</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {
+          this.props.navigator.push({
+            title: 'Toggles',
+            component: Toggles,
+          });
+        }}>
+          <Text style={styles.pushLabel}>Toggles</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    );
+
+}
+});
+
+var rnmkDemo = React.createClass({
+  render: function () {
+    return (
+      <NavigatorIOS
+        style={{flex:1}}
+        initialRoute={{
+          component: Home,
+          title: 'Examples',
+        }}
+      />
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  list: {
+    backgroundColor: '#F5FCFF',
+    paddingTop: 20,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginTop: 20, marginBottom: 0,
+  },
+  pushLabel: {
+    padding: 10,
+    color: '#2196F3',
+  }
+});
+
+AppRegistry.registerComponent('rnmkDemo', () => rnmkDemo);
